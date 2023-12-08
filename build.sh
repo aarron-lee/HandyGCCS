33b4160899d315480fcd7f267e32cc256eab6556
Aarron Lee <aclee91@gmail.com>
2023-12-08 17:54:38 -0500

    pull in original build scripts for chimera

--- build.sh
+++ build.sh
@@ -3,18 +3,5 @@
 python -m build --wheel --no-isolation
 sudo python -m installer dist/*.whl
 sudo cp -r usr/ /
-sudo rm /usr/bin/handycon
-sudo ln /usr/local/bin/handycon /usr/bin/handycon
 sudo systemd-hwdb update
 sudo udevadm control -R
-sudo systemctl disable handycon.service
-sleep 1
-sudo systemctl stop handycon.service
-sleep 1 
-sudo systemctl daemon-reload
-sleep 1 
-sudo systemctl enable handycon.service
-sleep 1
-sudo systemctl restart handycon.service
-sleep 3
-sudo systemctl status handycon.service
